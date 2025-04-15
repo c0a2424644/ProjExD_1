334f6b9eecbfd05121ce18110ae5a666e3d56908
c0a2424644 <c0a2424644@edu.teu.ac.jp>
2025-04-15 16:33:10 +0900

    練習１０：こうかとん操作

--- pygame_image.py
+++ pygame_image.py
@@ -13,16 +13,28 @@ def main():
     bg2_img = pg.transform.flip(bg_img, True, False)#練習８
     tori_img = pg.image.load("fig/3.png") #練習１
     tori_img = pg.transform.flip(tori_img, True, False)#練習２
+    tori_rct = tori_img.get_rect() #練習１０－１rect抽出
+    tori_rct.center = 300, 200 #練習１０ー２
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
+        key_lst = pg.key.get_pressed() #練習１０－３
+        #print(key_lst)
+        if key_lst[pg.K_UP]:#練習１０－４
+            tori_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            tori_rct.move_ip((0,1))
+        if key_lst[pg.K_RIGHT]:
+            tori_rct.move_ip((1,0))
+        if key_lst[pg.K_LEFT]:
+            tori_rct.move_ip((-1,0))
 
         x = tmr % 3200 #練習６
         screen.blit(bg_img, [-x, 0])#練習３
         screen.blit(bg2_img, [-x+1600,0])#練習７
         screen.blit(bg_img, [-x+3200,0])#練習９
-        screen.blit(tori_img, [300,200]) #練習４
+        screen.blit(tori_img, tori_rct) #練習４練習１０ー５
         pg.display.update()
         tmr += 1        
         clock.tick(200)#練習５
